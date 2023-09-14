64b7c7b4c255a0a7ffe97cfce028a3fd82aa46ca
Lisa <lisacjheinhold@gmail.com>
2023-09-13 20:49:28 -0600

    completed prep1 and prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
+// Lisa Heinhold 9/13/2023
+//Description: This project will ask the user to input their first and last name. It will then write back "your name is {lastName}, {firstName} {lastName}.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string lhFirst = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lhLast = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lhLast}, {lhFirst} {lhLast}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string lhPercentInput = Console.ReadLine();
+        float lhPercent = float.Parse(lhPercentInput);
+
+        string lhLetter = "";
+
+        if (lhPercent >= 90)
+        {   
+            lhLetter = "A";
+        }
+        else if (lhPercent >= 80 && lhPercent < 90)
+        {
+            lhLetter = "B";
+        }
+        else if (lhPercent >= 70 && lhPercent < 80)
+        {
+            lhLetter = "C";
+        }
+        else if (lhPercent >= 60 && lhPercent < 70)
+        {
+            lhLetter = "D";
+        }
+        else
+        {
+            lhLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {lhLetter}.");
+
+        if (lhPercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You are passing your class!");
+        }
+        else
+        {
+            Console.WriteLine("You are failing your class. You need to work harder.");
+        }
     }
 }
